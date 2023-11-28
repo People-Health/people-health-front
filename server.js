ac0c0310016eb01b3f15dc46074512cc5a46dd9a
Lipeeexxxx <101610971+Lipeeexxxx@users.noreply.github.com>
2023-11-27 22:30:30 -0300

    editando arquivo package.json e alterando tipo de arquivo para: server.js

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ import http from 'http';
 import { Server } from 'socket.io';
 //import io from 'socket.io-client';
 import cors from 'cors';
-
+import WebSocket from 'ws'
+ 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 //const socket = io('http://localhost:3000');
 
-const corsOptions: cors.CorsOptions = {
-  origin: 'http://localhost:3000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-};
+// const corsOptions: cors.CorsOptions = {
+//   origin: 'http://localhost:3000',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+// };
 
 app.use(cors(corsOptions));
 
