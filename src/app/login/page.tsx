7546854bb140e73c5b10e80adfb8082ca0bfe9cd
Lipeeexxxx <101610971+Lipeeexxxx@users.noreply.github.com>
2023-11-29 14:13:08 -0300

    textos e cores alteradas para preto na tela de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import style from  './login.module.css';
 import Footer from '../footer/footer';
-
+import Header from "../header/header";
 
 
 const  Login=()=>{
     return(
         <div className={style.container} style={{display:'flex'}}> {/*preciso dizer que o display é flex no css inline tbm */}
+        <Header option={0} />
             <main style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <LoginForm/>
             </main>
@@ -20,22 +21,25 @@ const  Login=()=>{
 
 const  LoginForm=()=>{
     return(
-   
         <form style={{display:'flex',flexDirection:'column',width:'300px'}}>
-            <label style={{display:'flex', justifyContent:'space-between'}}>
-                Usuário:
-                <input type='text' name="username" style={{marginBottom:'10px'}}/>
+            <label className={style.mensagemLogin}>
+                <h1>
+                    Faça seu login aqui!
+                </h1>
             </label>
-            <label style={{display:'flex', justifyContent:'space-between'}}>
+            <label className={style.usuario} style={{display:'flex', justifyContent:'space-between'}}>
+                Usuário: 
+                <input type='text' name="username" style={{marginBottom:'10px', color: 'black'}}/>
+            </label>
+            <label className={style.senha} style={{display:'flex', justifyContent:'space-between'}}>
                 Senha:
-                <input type="password"  name="password" style={{marginBottom:'10px'}}/>
+                <input type="password"  name="password" style={{marginBottom:'10px', color: 'black'}}/>
             </label>
             <div style={{display:'flex', justifyContent:'center'}}>
                 <button type="submit" style={{backgroundColor:'#1F2B6C',width:'70px'}}>Entrar</button>
             </div>
             
         </form>
-       
     )
 }
 
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,14 +1,17 @@
+"use client"
 import React from 'react';
-import Map from './map';
+//import Mapa from './map';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import Map from './map';
+
 
 const Home: React.FC = () => {
   return (
     <div>
       <Header option={1}/>
       <h1>Localização em tempo real de ambulâncias</h1>
-      <Map />
+      <Map/>
       <Footer/>
     </div>
   );
