a578ab9002b6978358f62d6cb159f3bd28812c54
Rafael <rafael.lqz@gmail.com>
2023-12-01 23:12:19 -0300

    Alterações Feitas

--- src/app/component.header/header.tsx
+++ src/app/component.header/header.tsx
@@ -1,67 +1,93 @@
-import style from './header.module.css';
-import ComercialPage from '../page';
-import Link from 'next/link';
-import Image from 'next/image';
-import logoRed from '../../../public/favicon.ico';
-import logo from '../../../public/logo.png';
-
+import style from "./header.module.css";
+import ComercialPage from "../page";
+import Link from "next/link";
+import Image from "next/image";
+import logoRed from "../../../public/favicon.ico";
+import logo from "../../../public/logo.png";
 
 type HeaderProps = {
-    option: number;
-  };
-  
-  const Header: React.FC<HeaderProps> = ({ option }) => {
-    if(option==0){//menu página comercial
-        return (
-            <header className={style.header}>
+  option: number;
+};
 
-                <div className={style.menuBtnsContainer}>
-                    <Image width={40} src={logoRed} style={{paddingRight:'10px'}} alt="logo"/>
-                    <button  className={style.button}><Link href="/">Início</Link></button>
-                    <button className={style.button}>Sobre nós</button>
-                    <button className={style.button} >Serviços</button>
-                    <button className={style.button}>Contato</button>
-                </div>
-                <button className={style.logInOutBtn}>
-                    <Link href="/login">Login</Link>
-                </button>
-            
-            </header>
-          );
+const Header: React.FC<HeaderProps> = ({ option }) => {
+  if (option == 0) {
+    //menu página comercial
+    return (
+      <header className={style.header}>
+        <div className={style.menuBtnsContainer}>
+          <Image
+            width={40}
+            src={logoRed}
+            style={{ paddingRight: "10px" }}
+            alt="logo"
+          />
+          <button className={style.button}>
+            <Link href="/">Início</Link>
+          </button>
+          <button className={style.button}>Sobre nós</button>
+          <button className={style.button}>Serviços</button>
+          <button className={style.button}>Contato</button>
+        </div>
+        <button className={style.logInOutBtn}>
+          <Link href="/login">Login</Link>
+        </button>
+      </header>
+    );
+  }
+  if (option == 1) {
+    //menu médico
+    return (
+      <header className={style.header}>
+        <div className={style.menuBtnsContainer}>
+          <Image
+            width={40}
+            src={logoRed}
+            style={{ paddingRight: "10px" }}
+            alt="logo"
+          />
+          <button className={style.button}>
+            <Link href="/">Início</Link>
+          </button>
+          <button className={style.button}>
+            <Link href="/search">Consultar Paciente</Link>
+          </button>
+          <button className={style.button}>
+            {" "}
+            <Link href="/register">Cadastrar Paciente</Link>
+          </button>
+          <button className={style.button}>Atualizar dados paciente</button>
+          <button className={style.button}>Mapa</button>
+        </div>
+        <button className={style.logInOutBtn}>
+          <Link href="/">Logout</Link>
+        </button>
+      </header>
+    );
+  }
+  if (option == 2) {
+    //menu paciente
 
-    }
-    if(option==1){//menu médico
-        return (
-            <header className={style.header}>
-                <div className={style.menuBtnsContainer}>
-                    <Image width={40} src={logoRed} style={{paddingRight:'10px'}}  alt="logo"/>
-                    <button className={style.button}><Link href="">Início</Link></button>
-                    <button className={style.button}>Consultar paciente</button>
-                    <button className={style.button}>Cadastrar paciente</button>
-                    <button className={style.button}>Atualizar dados paciente</button>
-                    <button className={style.button}>Mapa</button>
-                </div>
-                <button className={style.logInOutBtn}><Link href="/">Logout</Link></button>
-            </header>
-          );
-    }
-    if(option==2){//menu paciente
+    return (
+      <header className={style.header}>
+        <div className={style.menuBtnsContainer}>
+          <Image
+            width={40}
+            src={logoRed}
+            style={{ paddingRight: "10px" }}
+            alt="logo"
+          />
+          <button className={style.button}>
+            <Link href="">Início</Link>
+          </button>
+          <button className={style.button}>Exames</button>
+          <button className={style.button}>Dados médicos</button>
+        </div>
+        <button className={style.logInOutBtn}>
+          <Link href="/">Logout</Link>
+        </button>
+      </header>
+    );
+  }
+};
 
-        return(
-            <header className={style.header}>
-            <div className={style.menuBtnsContainer}>
-                <Image width={40} src={logoRed}  style={{paddingRight:'10px'}} alt="logo"/>
-                <button className={style.button}><Link href="">Início</Link></button>
-                <button className={style.button}>Exames</button>
-                <button className={style.button}>Dados médicos</button>
-            </div>
-            <button className={style.logInOutBtn}><Link href="/">Logout</Link></button>
-        </header>
-        );
-
-    }
-   
-  
-  };
-  
-  export default Header;
+export default Header;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,295 @@
+import Image from "next/image";
+import style from "../../app/register/register.module.css";
+import Link from "next/link";
+import Footer from "../component.footer/footer";
+import Header from "../component.header/header";
+import LogoFaixa from "../component.logoFaixa/logoFaixa";
+
+export default function register() {
+  return (
+    <>
+      <main className={style.container}>
+        <LogoFaixa />
+        <Header option={1} />
+        <label className={style.text1}>
+          <h1>Cadastrar Paciente</h1>
+        </label>
+        <label className={style.text2}>
+          <h1>Informações Pessoais:</h1>
+        </label>
+        <div>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1520px",
+            }}
+          >
+            Nome completo:
+            <input type="text" placeholder=" digite o nome aqui" name="nome" />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1600px",
+            }}
+          >
+            Idade:
+            <input
+              type="text"
+              placeholder=" digite a idade aqui"
+              name="idade"
+            />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1610px",
+            }}
+          >
+            CPF:
+            <input type="text" placeholder=" digite o cpf aqui" name="cpf" />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1620px",
+            }}
+          >
+            RG:
+            <input type="text" placeholder=" digite o rg aqui" name="rg " />
+          </label>
+          <label className={style.text2}>
+            <h1>Informações Clinicas:</h1>
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1455px",
+            }}
+          >
+            Medicação(ões) em uso:
+            <input
+              type="text"
+              placeholder=" digite o nome da(s) medicação(ções) aqui"
+              name="medicacao"
+            />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1565px",
+            }}
+          >
+            Alergia(s):
+            <input
+              type="text"
+              placeholder=" digite o nome da(s) alergia(s) aqui"
+              name="alergia"
+            />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1565px",
+            }}
+          >
+            Doença(s):
+            <input
+              type="text"
+              placeholder=" digite o nome da(s) doença(s) aqui"
+              name="doenca"
+            />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1535px",
+            }}
+          >
+            Deficiência(s):
+            <input
+              type="text"
+              placeholder=" digite o nome da(s) deficiência(s) aqui"
+              name="deficiencia"
+            />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1560px",
+            }}
+          >
+            Tabagismo:
+            <input type="text" placeholder=" sim ou não" name="tabagismo" />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1490px",
+            }}
+          >
+            Consumo de Alcool:
+            <input type="text" placeholder=" sim ou não" name="alcool" />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1529px",
+            }}
+          >
+            Uso de Drogas:
+            <input type="text" placeholder=" sim ou não" name="drogas" />
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1522px",
+            }}
+          >
+            Tipo Sanguíneo:
+            <input
+              type="text"
+              placeholder=" ex: A+, B-, O+, etc..."
+              name="sangue"
+            />
+          </label>
+          <label className={style.text2}>
+            <h1>Historico de Exames:</h1>
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1590px",
+            }}
+          >
+            Exame:
+            <input
+              type="text"
+              placeholder=" digite o tipo de exame feito"
+              name="exame"
+            />
+          </label>
+          <div>
+            <label
+              className={style.infos}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                padding: "10px",
+                width: "1590px",
+              }}
+            >
+              Upload do Exame:
+              <input type="file" accept="application/pdf" />
+            </label>
+          </div>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1525px",
+            }}
+          >
+            Data do exame:
+            <input
+              type="text"
+              placeholder=" digite a data do exame"
+              name="dataExame"
+            />
+          </label>
+          <label className={style.text2}>
+            <h1>Historico de Cirurgias:</h1>
+          </label>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1578px",
+            }}
+          >
+            Cirurgia:
+            <input
+              type="text"
+              placeholder=" digite o tipo de cirurgia feita"
+              name="cirurgia"
+            />
+          </label>
+          <div>
+            <label
+              className={style.infos}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                padding: "10px",
+                width: "1570px",
+              }}
+            >
+              Upload do Relatorio:
+              <input type="file" accept="application/pdf" />
+            </label>
+          </div>
+          <label
+            className={style.infos}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "10px",
+              width: "1521px",
+            }}
+          >
+            Data da Cirurgia:
+            <input
+              type="text"
+              placeholder=" digite a data da cirurgia"
+              name="dataCirurgia"
+            />
+          </label>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
